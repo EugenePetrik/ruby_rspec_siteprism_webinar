2765a82cf43f76abb7bad633814d0c276f853710
EugenePetrik <eugene.petrik@rubygarage.org>
2020-01-03 16:23:21 +0200

    Change Capybara settings

--- spec/support/config/capybara.rb
+++ spec/support/config/capybara.rb
@@ -2,97 +2,39 @@
 
 require 'capybara/rails'
 require 'capybara/rspec'
+require 'capybara-screenshot/rspec'
 require 'selenium-webdriver'
 
-# Register Chrome
-Capybara.register_driver(:selenium_chrome) do |app|
-  options = Selenium::WebDriver::Chrome::Options.new
-  options.add_argument('window-size=1600,1268')
-
-  # Loggers Values: "OFF", "SEVERE", "WARNING", "INFO", "CONFIG", "FINE", "FINER", "FINEST", "ALL"
-  # https://github.com/SeleniumHQ/selenium/wiki/DesiredCapabilities
-
+Capybara.register_driver(:chrome) do |app|
   CAPABILITIES = Selenium::WebDriver::Remote::Capabilities.chrome(
     loggingPrefs: {
-      browser: 'INFO', # Capture JavaScript errors in Browser
-      driver: 'INFO' # Capture WebDriver severe errors
+      browser: 'ALL',
+      client: 'ALL',
+      driver: 'ALL',
+      server: 'ALL'
     }
   )
 
-  Capybara::Selenium::Driver.new(
-    app,
-    browser: :chrome,
-    desired_capabilities: CAPABILITIES,
-    options: options
-  )
-end
-
-# Register Firefox
-Capybara.register_driver(:selenium) do |app|
-  options = Selenium::WebDriver::Firefox::Options.new
-  options.add_argument('window-size=1600,1268')
-
-  Capybara::Selenium::Driver.new(
-    app,
-    browser: :firefox,
-    options: options
-  )
-end
-
-# Register Chrome Headless
-Capybara.register_driver(:selenium_chrome_headless) do |app|
   options = Selenium::WebDriver::Chrome::Options.new
   options.add_argument('window-size=1600,1268')
-  options.add_argument('headless')
+  # Run headless by default unless CHROME_HEADLESS specified
+  options.add_argument('headless') unless /^(false|no|0)$/.match?(ENV['CHROME_HEADLESS'])
   options.add_argument('disable-gpu')
 
-  CAPABILITIES = Selenium::WebDriver::Remote::Capabilities.chrome(
-    loggingPrefs: {
-      browser: 'INFO', # Capture JavaScript errors in Browser
-      driver: 'INFO' # Capture WebDriver severe errors
-    }
-  )
-
   Capybara::Selenium::Driver.new(
     app,
     browser: :chrome,
     desired_capabilities: CAPABILITIES,
     options: options
   )
-end
-
-# Register Firefox Headless
-Capybara.register_driver(:selenium_headless) do |app|
-  options = Selenium::WebDriver::Firefox::Options.new
-  options.add_argument('--headless')
-  options.add_argument('--window-size=1600,1268')
-
-  Capybara::Selenium::Driver.new(
-    app,
-    browser: :firefox,
-    options: options
-  )
-end
-
-# Which default browser do you want Selenium WebDriver to use?
-# DRIVER=selenium_chrome => Selenium driving Chrome
-# DRIVER=selenium_chrome_headless => Selenium driving Chrome in a headless configuration
-# DRIVER=selenium => Selenium driving Firefox
-# DRIVER=selenium_headless => Selenium driving Firefox in a headless configuration
-# https://github.com/teamcapybara/capybara#selenium
-if ENV['DRIVER']
-  driver = ENV['DRIVER'].to_sym
-  Capybara.default_driver = driver
-  Capybara.javascript_driver = driver
-else
-  Capybara.default_driver = :selenium_chrome_headless
-  Capybara.javascript_driver = :selenium_chrome_headless
 end
 
 Capybara.configure do |config|
   config.always_include_port = true
   config.asset_host = 'http://localhost:3000'
-  config.default_max_wait_time = 3
+  config.default_driver = :chrome
+  config.javascript_driver = :chrome
+  config.default_max_wait_time = 5
   config.enable_aria_label = true
   config.ignore_hidden_elements = true
   config.server = :puma, { Silent: true }
@@ -108,11 +50,15 @@ RSpec.configure do |config|
 
   config.after(:suite) do
     browser_errors = Capybara.page.driver.browser.manage.logs.get(:browser)
+    client_errors = Capybara.page.driver.browser.manage.logs.get(:client)
     driver_errors = Capybara.page.driver.browser.manage.logs.get(:driver)
+    server_errors = Capybara.page.driver.browser.manage.logs.get(:server)
 
     Dir.mkdir('tmp/logs') unless Dir.exist?('tmp/logs')
 
     open('tmp/logs/chrome.log', 'w') { |f| f <<  browser_errors }
+    open('tmp/logs/client.log', 'w') { |f| f <<  client_errors }
     open('tmp/logs/chromedriver.log', 'w') { |f| f << driver_errors }
+    open('tmp/logs/server.log', 'w') { |f| f << server_errors }
   end
 end
