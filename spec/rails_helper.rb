7c5a51d57811b17b0f9d146d6d0bb2379094b285
EugenePetrik <eugene.petrik@rubygarage.org>
2020-01-14 20:03:06 +0200

    Move ActiveRecord::Migration to support folder

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -17,9 +17,6 @@ require 'rspec/rails'
 # Load site prism page objects
 require File.expand_path('support/pages/base_page', __dir__)
 
-# Load modules under the spec/support directory
-Dir.glob(File.expand_path('support/**/*.rb', __dir__), &method(:require))
-
 # Add additional requires below this line. Rails is not loaded until this point!
 
 # Requires supporting ruby files with custom matchers and macros, etc, in
@@ -34,17 +31,9 @@ Dir.glob(File.expand_path('support/**/*.rb', __dir__), &method(:require))
 # of increasing the boot-up time by auto-requiring all files in the support
 # directory. Alternatively, in the individual `*_spec.rb` files, manually
 # require only the support files necessary.
-#
-# Dir[Rails.root.join('spec', 'support', '**', '*.rb')].each { |f| require f }
 
-# Checks for pending migrations and applies them before tests are run.
-# If you are not using ActiveRecord, you can remove these lines.
-begin
-  ActiveRecord::Migration.maintain_test_schema!
-rescue ActiveRecord::PendingMigrationError => e
-  puts e.to_s.strip
-  exit 1
-end
+# Load modules under the spec/support directory
+Dir[Rails.root.join('spec', 'support', '**', '*.rb')].each { |f| require f }
 
 RSpec.configure do |config|
   # Remove this line if you're not using ActiveRecord or ActiveRecord fixtures
